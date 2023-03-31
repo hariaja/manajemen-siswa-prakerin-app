2eae18a87d299f5609d684ce8330b5202943fb9d
aldiamahari16 <aldiamahari16@gmail.com>
2023-03-31 22:24:07 +0700

    Initial Commit

---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+  use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  /**
+   * The accessors to append to the model's array form.
+   *
+   * @var array
+   */
+  // protected $appends = [
+  //   'avatar',
+  // ];
+
+  // public function getAvatarAttribute()
+  // {
+  //   return Storage::url($this->attributes['avatar']);
+  // }
+
+  public function getAvatar()
+  {
+    if (!$this->avatar) :
+      return asset('assets/images/default.png');
+    else :
+      return Storage::url($this->avatar);
+    endif;
+  }
+}
